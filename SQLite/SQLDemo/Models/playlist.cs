e0630c9b23c29d85c04add3eeaf6b7f65f0339f7
yvanvds <yvan@attr-x.net>
2020-01-12 19:06:38 +0100

    first commit

---
+++ SQLite/SQLDemo/Models/playlist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SQLite.Linq;
+
+namespace SQLDemo.Models
+{
+
+    class playlist
+    {
+    }
+}
---
+++ SQLite/SQLDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Data.SQLite.Linq;
+using System.Linq;
+
+namespace SQLDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new SQLiteConnection("URI=file:chinook.db; FailIfMissing = True");
+            connection.Open();
+
+            string query = "SELECT name FROM sqlite_master WHERE type = 'table' ORDER BY 1";
+
+            var command = new SQLiteCommand(query, connection);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            Console.WriteLine("Results");
+
+            while(reader.Read())
+            {
+                Console.WriteLine($"{reader.GetString(0)}");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Results with linq");
+
+            connection.Table<
+
+            Console.ReadLine();
+            connection.Close();
+        }
+    }
+}
